bb23edad3a3775198032a589dcac3574ed8ea762
Shreyansh Shrivastva <shreyanshshrivastva@gmail.com>
2025-01-15 20:37:29 +0530

    Another prod bug fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+"use client";
+
+import dynamic from 'next/dynamic';
 import Image from "next/image";
 
 import bgImg from "/public/images/image 10.png";
 
-import Navbar from "./components/(homepage)/Navbar";
+const Navbar = dynamic(() => import('./components/(homepage)/Navbar'), {
+    ssr: false
+});
+
 import Hero from "./components/(homepage)/Hero";
 import About from "./components/(homepage)/About";
 import Skills from "./components/(homepage)/Skills";
